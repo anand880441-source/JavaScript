4a6c2c1257b1d794fc46822410ffe4a5be18ffac
Anand Suthar <anand880441@gmail.com>
2025-12-17 18:58:41 +0530

    edit: new comment and some changes

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,35 +1,47 @@
-const name = "hitesh"
+const name = "spino"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('spino-cg-com') // new -> using object
+// console.log(typeof gameName);  // type => object
+
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// console.log(gameName.__proto__) // Out : {}
 
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+console.log(gameName.charAt(2));  //to check what is at the second index
+console.log(gameName.indexOf('h'));  //to check the value at perticular indrex
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4) //it will give spin (up to index 3), 4 is not include  -> negative value is not allow if we give it still start from 0.
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-7, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+
+/*
+Simple Comparison Table for substring() and slice().
+Feature	                 substring()	         slice()
+Negative indices	     Treated as 0	         Counts from end of string
+start > end	             Swaps parameters	     Returns empty string
+Method origin	         String-specific	     Also works with arrays
+Usage with arrays	     ❌ No	               ✅ Yes
+*/
+
+const newStringOne = "   spino    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://hitesh.com/onepwr%20spino"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('divon'))
 
 console.log(gameName.split('-'));
