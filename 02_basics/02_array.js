0af451b1feb42f4d9f6129afceb6789b3620dd88
Anand Suthar <anand880441@gmail.com>
2025-12-19 21:08:28 +0530

    some comment message added

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,31 +1,31 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)   //this will create nested array in marval_heros
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros)   // Combines two. This method returns a new array without modifying any existing arrays.
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros]  //// Combines two or more arrays. This method returns a new array without modifying any existing arrays.
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+const real_another_array = another_array.flat(Infinity)    //Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+// console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))   //Creates an array from an iterable object.
+// console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));   //Returns a new array from a set of elements.
