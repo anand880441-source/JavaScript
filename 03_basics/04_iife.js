a864adfc1bf57bcd6051360dc3d5aa3a685bc615
Anand Suthar <anand880441@gmail.com>
2025-12-21 11:45:10 +0530

    slightly changes and new comment added

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,13 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+//->a JavaScript function that runs as soon as it is defined. 
+//   It's a common pattern used primarily to create a local scope for variables to prevent them from polluting the global namespace. 
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();  //-> ";" -> used to stop.
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('SPINO')
 
