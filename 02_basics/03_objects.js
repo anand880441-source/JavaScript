67e338e77d049057f5fb74454cb6f083670a5549
Anand Suthar <anand880441@gmail.com>
2025-12-21 10:39:59 +0530

    comment message added

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,15 +1,15 @@
 // singleton
-// Object.create
+// Object.create //-> a singleton meathod
 
 // object literals
 
 const mySym = Symbol("key1")
-
+// console.log(mySym);
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    name: "SPINO",
+    "full name": "ONWPOWER SPINO",  // -> this type of key cannot be accessable with "." , thats why we use square notation to excess that value "["key_name"]"
+    [mySym]: "mykey1",    //-> using symbol as a key
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -18,16 +18,16 @@ const JsUser = {
 }
 
 // console.log(JsUser.email)
-// console.log(JsUser["email"])
+// console.log(JsUser["email"])  //-> alternate way to excess.
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser)  //-> by freezeing the value it could not be changed.
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
-JsUser.greeting = function(){
+JsUser.greeting = function(){     
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
